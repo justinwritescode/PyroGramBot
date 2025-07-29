a833cea362dccf38b4e0208d83b818a22913b47c
shriMADhav U k <hetzner@shrimadhavuk.me>
2025-07-29 17:48:13 +0200

    Refactor get_file_id function and update showid to include DC ID in response

--- pyrobot/helper_functions/get_file_id.py
+++ pyrobot/helper_functions/get_file_id.py
@@ -5,23 +5,12 @@ from pyrogram.types.messages_and_media import message
 
 
 def get_file_id(msg: Message):
+    if not msg:
+        return None
     if msg.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            # "contact",
-            # "dice",
-            # "poll",
-            # "location",
-            # "venue",
-            "sticker",
-        ):
-            obj = getattr(msg, message_type)
-            if obj:
-                setattr(obj, "message_type", message_type)
-                return obj
+        obj = getattr(msg, msg.media.value, None)
+        if obj:
+            setattr(obj, "message_type", msg.media.value)
+            setattr(obj, "message_raw", msg._raw.media)
+            return obj
+    return None
--- pyrobot/plugins/admemes/show_file_id.py
+++ pyrobot/plugins/admemes/show_file_id.py
@@ -2,6 +2,7 @@
 Syntax: .id"""
 
 from pyrogram import Client, filters
+from pyrogram.file_id import FileId
 from pyrogram.enums import ChatType
 from pyrobot import COMMAND_HAND_LER
 from pyrobot.helper_functions.cust_p_filters import f_onw_fliter
@@ -13,12 +14,18 @@ async def showid(client, message):
     chat_type = message.chat.type
 
     if chat_type == ChatType.PRIVATE:
-        user_id = message.chat.id
-        await message.reply_text(f"<code>{user_id}</code>", quote=True)
+        _id = ""
+        _id += f"<b>User ID</b>: <code>{message.from_user.id}</code>\n"
+        file_info = get_file_id(message.reply_to_message)
+        if file_info:
+            file_id = file_info.file_id
+            d_file_id = FileId.decode(file_id)
+            _id += f"<b>DC ID</b>: <code>{d_file_id.dc_id}</code>\n"
+        await message.reply_text(_id, quote=True)
 
     elif chat_type in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
         _id = ""
-        _id += "<b>Chat ID</b>: " f"<code>{message.chat.id}</code>\n"
+        _id += f"<b>Chat ID</b>: <code>{message.chat.id}</code>\n"
         if message.reply_to_message:
             _id += (
                 "<b>Replied User ID</b>: "
@@ -30,12 +37,16 @@ async def showid(client, message):
                     f"<b>{file_info.message_type}</b>: "
                     f"<code>{file_info.file_id}</code>\n"
                 )
+                d_file_id = FileId.decode(file_id)
+                _id += f"<b>DC ID</b>: <code>{d_file_id.dc_id}</code>\n"
         else:
-            _id += "<b>User ID</b>: " f"<code>{message.from_user.id}</code>\n"
+            _id += f"<b>User ID</b>: <code>{message.from_user.id}</code>\n"
             file_info = get_file_id(message)
             if file_info:
                 _id += (
                     f"<b>{file_info.message_type}</b>: "
                     f"<code>{file_info.file_id}</code>\n"
                 )
+                d_file_id = FileId.decode(file_id)
+                _id += f"<b>DC ID</b>: <code>{d_file_id.dc_id}</code>\n"
         await message.reply_text(_id, quote=True)
