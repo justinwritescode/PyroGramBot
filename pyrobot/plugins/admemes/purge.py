a9a6ac8403a9e6105598b1c0b5d903aa4e77371a
shriMADhav U k <hetzner@shrimadhavuk.me>
2026-03-03 15:54:44 +0100

    feat: Add /purge support in forum topic chats

--- pyrobot/plugins/admemes/purge.py
+++ pyrobot/plugins/admemes/purge.py
@@ -21,20 +21,32 @@ async def purge(client, message):
     count_del_etion_s = 0
 
     if message.reply_to_message:
-        for a_s_message_id in range(
-            message.reply_to_message.id, message.id
-        ):
-            message_ids.append(a_s_message_id)
-            if len(message_ids) == TG_MAX_SELECT_LEN:
-                count_del_etion_s += await client.delete_messages(
-                    chat_id=message.chat.id, message_ids=message_ids, revoke=True
-                )
-                message_ids = []
+        if message.chat.is_forum:
+            for a_s_message_id in range(
+                message.reply_to_message.id, message.id
+            ):
+                M = await client.get_messages(message.chat.id, a_s_message_id)
+                if M.message_thread_id == message.message_thread_id:
+                    message_ids.append(a_s_message_id)
+                if len(message_ids) == TG_MAX_SELECT_LEN:
+                    count_del_etion_s += await client.delete_messages(
+                        chat_id=message.chat.id, message_ids=message_ids, revoke=True
+                    )
+                    message_ids = []
+        else:
+            for a_s_message_id in range(
+                message.reply_to_message.id, message.id
+            ):
+                message_ids.append(a_s_message_id)
+                if len(message_ids) == TG_MAX_SELECT_LEN:
+                    count_del_etion_s += await client.delete_messages(
+                        chat_id=message.chat.id, message_ids=message_ids, revoke=True
+                    )
+                    message_ids = []
         if len(message_ids) > 0:
             count_del_etion_s += await client.delete_messages(
                 chat_id=message.chat.id, message_ids=message_ids, revoke=True
             )
-
     await status_message.edit_text(f"deleted {count_del_etion_s} messages")
     await asyncio.sleep(5)
     await status_message.delete()
